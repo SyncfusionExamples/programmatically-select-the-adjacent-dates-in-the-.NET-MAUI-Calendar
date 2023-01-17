11ab3de3aa55aad0013de70e8f73c641f649699e
VishalOmprasad <vishal.omprasad@syncfusion.com>
2023-01-17 16:07:19 +0530

    New KB sample added

---
+++ AdjacentDatesNavigation/AdjacentDatesNavigation/Behavior/CalendarBehavior.cs
@@ -0,0 +1,59 @@
+﻿using Syncfusion.Maui.Calendar;
+
+namespace AdjacentDatesNavigation
+{
+    public class CalendarBehavior : Behavior<ContentPage>
+    {
+        private SfCalendar sfCalendar;
+        private Button button;
+        
+        protected override void OnAttachedTo(ContentPage bindable)
+        {
+            base.OnAttachedTo(bindable);
+            this.sfCalendar = bindable.FindByName<SfCalendar>("calendar");
+            if (this.sfCalendar.SelectedDate == null)
+            {
+                this.sfCalendar.SelectedDate = this.sfCalendar.DisplayDate;
+            }
+
+            this.sfCalendar.SelectionChanged += SfCalendar_SelectionChanged;
+            this.button = bindable.FindByName<Button>("NavigateButton");
+            this.button.Clicked += Button_Clicked;
+        }
+
+        private void SfCalendar_SelectionChanged(object sender, CalendarSelectionChangedEventArgs e)
+        {
+            DateTime selectedDate = DateTime.Parse(e.NewValue.ToString());
+            if (selectedDate.Month != this.sfCalendar.DisplayDate.Month)
+            {
+                sfCalendar.DisplayDate = selectedDate;
+            }
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            if (this.sfCalendar.SelectedDate != null)
+            {
+                this.sfCalendar.SelectedDate = this.sfCalendar.SelectedDate.Value.AddDays(1);
+            }
+        }
+            
+
+        protected override void OnDetachingFrom(ContentPage bindable)
+        {
+            base.OnDetachingFrom(bindable);
+            if (this.sfCalendar != null)
+            {
+                this.sfCalendar.SelectionChanged -= SfCalendar_SelectionChanged;
+            }
+
+            if (this.button != null)
+            {
+                this.button.Clicked -= Button_Clicked;
+            }
+
+            this.sfCalendar = null;
+            this.button = null;
+        }
+    }
+}
